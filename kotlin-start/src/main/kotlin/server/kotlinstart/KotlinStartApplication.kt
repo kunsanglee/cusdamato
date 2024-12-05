2aa60dde4da6fa115543d27407bbb7592b5904f2
kunsanglee <gosmdochee2@gmail.com>
2024-12-05 18:24:20 +0900

    feat: kotlin-start init

---
+++ kotlin-start/src/main/kotlin/server/kotlinstart/KotlinStartApplication.kt
@@ -0,0 +1,11 @@
+package server.kotlinstart
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinStartApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinStartApplication>(*args)
+}
---
+++ kotlin-start/src/test/kotlin/server/kotlinstart/KotlinStartApplicationTests.kt
@@ -0,0 +1,13 @@
+package server.kotlinstart
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinStartApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
