ef9468f09741be4440f3519a7687ecea67e8c5f7
kunsanglee <gosmdochee2@gmail.com>
2024-12-05 17:50:31 +0900

    feat: spring-batch-in-action initial commit

--- spring-batch-in-action/src/main/java/server/springbatchinaction/SpringBatchInActionApplication.java
+++ spring-batch-in-action/src/main/java/server/springbatchinaction/SpringBatchInActionApplication.java
@@ -1,8 +1,10 @@
 package server.springbatchinaction;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableBatchProcessing // 배치 기능 활성
 @SpringBootApplication
 public class SpringBatchInActionApplication {
 
